78804b133754a80d5611be54bf22497480236798
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-08-30 17:59:44 +0300

    Add nodes on the collection cards div

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,19 +8,26 @@ function fetchNftData() {
                 const collectionCards = document.querySelector('.collection-cards')
                 const div = document.createElement('div');
                 div.className = 'collection-card'
+                const link = document.createTextNode('Collection Link')
                 const image = document.createElement('img');
                 const h4 = document.createElement('h4');
                 const aHref = document.createElement('a');
 
                 image.src = collection.collection_image;
-                div.appendChild(image)
-                collectionCards.appendChild(div)
+                h4.textContent = `${collection.collection_name}`;
+                aHref.href = `${collection.Collection_url}`
+                aHref.appendChild(link)
 
-                console.log(image)
+                div.appendChild(image);
+                div.appendChild(h4);
+                div.appendChild(aHref)
+                collectionCards.appendChild(div);
+
+                console.log(aHref)
                 
             });
         })
-        .catch(err => console.log(err.message))
+        .catch(err => console.log(err))
 
 
 }
