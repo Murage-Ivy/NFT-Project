7acc18735dd41b5f18469cfc74ee29f0f3c9d11a
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-08-30 17:52:37 +0300

    Create fetch request for the nft collection

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,34 +1,28 @@
 function fetchNftData() {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'e7e7730bf7mshbeec5b1c0cc217bp1faa6ajsn05a7a5785127',
-            'X-RapidAPI-Host': 'top-nft-collections-and-sales.p.rapidapi.com'
-        }
-    };
-
-    return fetch('https://top-nft-collections-and-sales.p.rapidapi.com/collections/30d', options)
-        .then(response => response.json())
-        .then(response => {
-            const newsCards = document.querySelector('.collection-card');
-            console.log(newsCards)
-
-
-
-            for (let i = 0; i <= 7; i++) {
-                const img = document.createElement('img')
-                const a_href = document.createElement('a');
+    const apiHost = '  http://localhost:3000'
+    return fetch(`${apiHost}/collections`)
+        .then(res => res.json())
+        .then(collections => {
+            console.log(collections);
+            collections.forEach(collection => {
+                const collectionCards = document.querySelector('.collection-cards')
+                const div = document.createElement('div');
+                div.className = 'collection-card'
+                const image = document.createElement('img');
                 const h4 = document.createElement('h4');
-                let link = document.createTextNode(`${response[i].collection_name}`);
-                a_href.appendChild(link)
-                a_href.href = `${response[i].collection_url}`
-                a_href.title = `${response[i].collection_name}`
-                console.log(response[i].collection_url)
-                document.querySelector('.collection-card').appendChild(a_href)
-            }
+                const aHref = document.createElement('a');
 
+                image.src = collection.collection_image;
+                div.appendChild(image)
+                collectionCards.appendChild(div)
+
+                console.log(image)
+                
+            });
         })
-        .catch(err => console.error(err));
+        .catch(err => console.log(err.message))
+
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
