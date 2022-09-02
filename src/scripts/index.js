a63e14e37a63da60d784c7cde7ed45f0a7879d33
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-09-02 14:29:57 +0300

    Add comments to the index.js file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,37 +1,57 @@
 function fetchNftData() {
-    const apiHost = '  http://localhost:3000'
+    const apiHost = '  http://localhost:3000';
+
     return fetch(`${apiHost}/collections`)
         .then(res => res.json())
         .then(collections => {
             console.log(collections);
             collections.forEach(collection => {
+                // grab the collections-cards div to store cards
                 const collectionCards = document.querySelector('.collection-cards')
                 const div = document.createElement('div');
                 div.className = 'collection-card'
+
+                // create new elements to store nft information
                 const link = document.createTextNode('Collection Link');
                 const image = document.createElement('img');
                 const h4 = document.createElement('h4');
                 const aHref = document.createElement('a');
+                const button = document.createElement('button');
+                const span = document.createElement('span');
+                span.className = 'likes'
+                button.className = 'like-button'
 
+
+
+                // add textnodes between the tags
                 image.src = collection.collection_image;
                 h4.textContent = `${collection.collection_name}`;
-                aHref.href = `${collection.Collection_url}`
+                aHref.href = `${collection.Collection_url}`;
+                button.innerHTML = `<i class="fa-solid fa-heart"></i>`;
+                span.textContent = '0 likes'
                 aHref.appendChild(link)
 
+                // append the div cards on the collection-cards div
                 div.appendChild(image);
                 div.appendChild(h4);
-                div.appendChild(aHref)
+                div.appendChild(aHref);
+                div.appendChild(button)
+                div.appendChild(span)
                 collectionCards.appendChild(div);
 
-                console.log(aHref)
 
             });
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            console.log(err)
+            collectionCards.textContent = `${err.message}`
+
+        })
 
 
 }
 
+// when a user clicks the submit button the input fields are cleared
 function submitData() {
     const form = document.querySelector('.contact-form');
     console.log(form)
@@ -58,13 +78,14 @@ function submitData() {
 
 function joinFunction() {
     const button = document.querySelector('#join-btn');
-
+    // adds an event listener when the user clicks the join us button
+    // when user clicks it the a text message is displayed 
     button.addEventListener('click', () => {
         alert('Fill the contact form to Join the Nefte Club');
 
     })
 }
-
+// on load of the dom the following functions are called 
 document.addEventListener('DOMContentLoaded', () => {
     fetchNftData()
     submitData()
